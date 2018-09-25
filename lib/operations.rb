58ef5834fdfd832d4475a0fd467678ff0be73e02
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-25 18:32:07 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,9 @@
 def unsafe?(speed)
-  if speed > 60 && speed < 40 || if speed.between?(40, 60)
+  if speed > 60 && speed < 40 || speed.between?(40, 60)
     true
   else 
     false
   end
-end
 end
 
 def not_safe?(speed)
